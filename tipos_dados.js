5e3a2aa614eff7fd2376801f0db9d2cf236e2261
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-01-02 13:21:47 -0300

    tipos de dados

---
+++ tipos_dados.js
@@ -0,0 +1,33 @@
+//numbers
+let length = 16;
+let weight = 7.5;
+
+//string
+let color = "yellow";
+let name = "johson";
+
+//booleans
+let x = true;
+let y = false;
+
+//object
+const person = {
+    firstName: "john",
+    lastName: "doe"
+};
+
+//array 
+const car = [
+    "saab",
+    "volvo",
+    "bmw"
+]
+
+//date object
+const date = new Date("2025-03-25");
+
+//retorna o tipo de variavel
+
+typeof "john" //returns "string"
+typeof 0 // returns "numebr"
+
